a8e7b114a4b247964425d4254fd21c1cadc37cac
anaraa@code.edu.az <you@example.com>
2023-02-06 12:41:43 -0800

    Js

---
+++ script.js
@@ -0,0 +1,181 @@
+"use strict"
+
+// var surnane = "Ismayilzade" ;
+
+// let name = "Cavid" ;
+
+// const IS_MARRIED = true;
+
+
+// var surname = "ismayilzade" ;
+// var name = " Cavid" ;
+
+// console.log(surname + " " +name)
+
+
+// if(true){
+//     var age = 55;
+
+// }
+
+// console.log(age)
+
+// if(true){
+//     let age = 55;
+//     console.log(age)
+
+// }
+
+// const age = 55;
+
+// console.log (age)
+
+// let age =17;
+
+// let name = "Cavid" ;
+
+// let isMarried = false;
+
+// let address;
+
+// let phone = null;
+
+
+
+
+
+// console.log(typeof age)
+// console.log(typeof name)
+// console.log(typeof isMarried)
+// console.log(typeof address)
+// console.log(typeof phone)
+
+
+// if(address == undefined){
+//     console.log("addres undefined ")
+
+// }
+
+// if(phone == null){
+//     console.log("addres null oldu")
+// }
+
+// let stu = {
+//     name:"Cavid" ,
+//     surname:"Imayilzade"
+
+// }
+
+// console.log(stu)
+
+// console.log(stu.name + " " + stu.surname)
+// console.log(`${stu.name} ${stu.surname} `);
+
+// let students = ["Cavid","pervin","Mirze",100,200,true];
+
+// for(let i = 0; i < students.length;i++){
+//     console.log(students[i])
+// }
+
+
+// console.log("Cavid"/3)
+
+
+// let address="66"
+
+// alert("salamlar")
+
+
+// let username = prompt("Pleace ad your username")
+
+// console.log(username)
+
+// let isOnline = confirm("Is he online")
+
+// if(isOnline){
+//     console.log("He is online")
+
+// }else{
+
+//     console.log("He is ofline")
+// }
+
+// debugger
+
+// console.log(isOnline)
+
+
+// let age = 20;
+
+// if(age > 15){
+//     console.log("Boyukdur")
+
+// }else{
+//     console.log("Kicikdir")
+// }
+
+// let temp = 25;
+
+// if (temp > 10) {
+//     console.log("Normaldir")
+// } else if (temp > 20) {
+//     console.log("istidir")
+// } else {
+//     console.log("Hecbisey")
+// }\\
+
+
+// let count = 66;
+
+// switch (count) {
+//     case 5:
+//         console.log("besh")
+//         break;
+//     case 6:
+//         console.log("alti")
+//         break;
+//     default:
+//         console.log("notfound")
+// }
+
+// for(let i = 0; i<20;i++){
+//     console.log(i)
+// }
+
+
+// let result = 2**3;
+
+// console.log(result)
+
+// console.log(100/10)
+
+// console.log(10*10)
+
+// console.log("44"/2)
+
+// console.log("44c"/2)
+
+
+// let isMarried = true;
+
+// let result = isMarried?"Evlidir":"Subaydir";
+
+// console.log(result)
+
+let address = null;
+
+// let result = address??"Ehmedli";
+
+// console.log(result);
+
+// if(address == null){
+//     console.log("Ehmedli")
+// }else{
+//     console.log(address)
+// }
+
+let res = address == null ? "Ehmedli" :address;
+
+console.log(res)
+
+
